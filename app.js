c06d68c20dab608bab69ce2bfce517e9ec8251ec
Ian Macalinao <me@ian.pw>
2015-04-04 15:30:49 -0500

    Send responsE

--- app.js
+++ app.js
@@ -12,14 +12,17 @@ app.get('/status', function(req, res) {
 
 app.post('/on', function(req, res) {
   status = true;
+  res.json(1);
 });
 
 app.post('/off', function(req, res) {
   status = false;
+  res.json(1);
 });
 
 app.post('/toggle', function(req, res) {
   status = !status;
+  res.json(1);
 });
 
 var port = process.env.PORT || 3000;
