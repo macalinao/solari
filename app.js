cb482e973426c42b9ea5b6cee8cdc28e1e0b5383
Ian Macalinao <me@ian.pw>
2015-04-03 23:30:30 -0500

    Add app toggle on and off

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ var app = express();
 
 app.use(express.static('public/'));
 
+var status = false;
+
+app.get('/status', function(req, res) {
+  res.json(status);
+});
+
+app.post('/on', function(req, res) {
+  status = true;
+});
+
+app.post('/off', function(req, res) {
+  status = false;
+});
+
+app.post('/toggle', function(req, res) {
+  status = !status;
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Listening on port ' + port);
